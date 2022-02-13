ecd44cb90539d4ef780238462f531f0c7e2bc765
AnatoliyIliev <vekok12@gmail.com>
2022-02-13 19:37:47 +0200

    morse-decode

--- src/index.js
+++ src/index.js
@@ -1,46 +1,75 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
-}
+  const exp = expr.split('');
 
-module.exports = {
-    decode
+  let tenNum = [];
+  for (let i = 0; i < exp.length; i += 10) {
+    tenNum.push(exp.slice(i, i + 10));
+  }
+
+  let twoNum = tenNum.map(a => {
+    let res = [];
+    for (let i = 0; i < a.length; i += 2) {
+      res.push(a.slice(i, i + 2).join(''));
+    }
+    return res;
+  });
+
+  let morseCode = twoNum
+    .map(arr =>
+      arr.map(num => {
+        if (num === '00') return '';
+        else if (num === '10') return '.';
+        else if (num === '11') return '-';
+        else if (num === '**') return '0';
+      }),
+    )
+    .map(code => code.reduce((acc, prev) => acc + prev, []))
+    .map(letter => {
+      if (letter === '00000') return ' ';
+      else return MORSE_TABLE[letter];
+    });
+
+  return morseCode.join('');
 }
+module.exports = {
+  decode,
+};
